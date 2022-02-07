c4ffc487720588dcdf70b5a763475e4a3142a92a
nsk6 <nsk@axofinans.no>
2022-02-07 08:22:33 +0100

    Added fish config

---
+++ config.fish
@@ -0,0 +1,14 @@
+set fish_greeting
+
+# Various commands
+alias py3="python3"
+alias o="ranger"
+alias tmux="tmux -2"
+
+alias vc="code ."
+
+# Keybinds
+fish_vi_key_bindings
+function fish_mode_prompt; end
+
+fish_ssh_agent
