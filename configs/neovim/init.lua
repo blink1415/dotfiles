61fc759bcf845fa84d343c552ee8d4714d653fff
Nikolai Steen Kjosnes <nsk@axofinance.com>
2022-10-10 15:41:47 +0200

    Neovim 8.0 compatibility changes

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -3,7 +3,7 @@ local config = {
 
   updater = {
     remote = "origin",
-    channel = "nightly",
+    channel = "stable",
     version = "latest",
     branch = "main",
     commit = nil,
@@ -72,6 +72,9 @@ local config = {
     servers = {
       "sumneko_lua"
     },
+    formatting = {
+      format_on_save = false, -- enable or disable auto formatting on save
+    }
   },
 
   mappings = {
@@ -128,7 +131,7 @@ local config = {
         end,
       },
 
-      { "ray-x/go.nvim", 
+      { "ray-x/go.nvim",
         config = function()
           require('go').setup({
 
@@ -262,41 +265,12 @@ local config = {
       { "theHamsta/nvim-dap-virtual-text" },
 
     },
-    -- All other entries override the require("<key>").setup({...}) call for default plugins
-    ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
-      -- config variable is the default configuration table for the setup functino call
-      local null_ls = require "null-ls"
-      -- Check supported formatters and linters
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-      config.sources = {
-        -- Set a formatter
-        -- null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.formatting.prettier,
-      }
-      -- set up null-ls's on_attach function
-      -- NOTE: You can remove this on attach function to disable format on save
-      config.on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            desc = "Auto format before save",
-            pattern = "<buffer>",
-            callback = vim.lsp.buf.formatting_sync,
-          })
-        end
-      end
-      return config -- return final config table to use in require("null-ls").setup(config)
-    end,
     treesitter = { -- overrides `require("treesitter").setup(...)`
       ensure_installed = { "lua" },
     },
     -- use mason-lspconfig to configure LSP installations
     ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
       ensure_installed = { "sumneko_lua" },
-    },
-    -- use mason-tool-installer to configure DAP/Formatters/Linter installation
-    ["mason-tool-installer"] = { -- overrides `require("mason-tool-installer").setup(...)`
-      -- ensure_installed = { "prettier", "stylua" },
     },
     packer = { -- overrides `require("packer").setup(...)`
       compile_path = vim.fn.stdpath "data" .. "/packer_compiled.lua",
